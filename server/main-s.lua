d923369f03eaf78b80c945f2a43c46805fda2dd5
Diaphanous (TGB) <65540686+techygamebar@users.noreply.github.com>
2025-06-10 20:01:22 +0530

    Heirarchial distribution

--- server/main-s.lua
+++ server/main-s.lua
@@ -37,11 +37,30 @@
 
 
 
-RegisterCommand("additem", function (source,args,raw)
-local identifier = GetPlayerIdentifiers(source)[1]
-   print(args[1])
-	TriggerClientEvent("inventory:additem",source , args[1])
+RegisterCommand("additem", function(source, args, raw)
+    -- Ensure proper arguments are given
+    if not args[1] or not args[2] then
+        TriggerClientEvent("chat:addMessage", source, {
+            args = {"^1SYSTEM", "Usage: /additem [playerID] [itemName]"}
+        })
+        return
+    end
 
+    local targetId = tonumber(args[1])
+    local itemName = args[2]
+
+    if targetId and GetPlayerIdentifiers(targetId)[1] then
+        TriggerClientEvent("inventory:additem", targetId, itemName)
+
+        -- Feedback to command issuer
+        TriggerClientEvent("chat:addMessage", source, {
+            args = {"^2SYSTEM", "Item '" .. itemName .. "' given to player ID " .. targetId}
+        })
+    else
+        TriggerClientEvent("chat:addMessage", source, {
+            args = {"^1SYSTEM", "Invalid player ID."}
+        })
+    end
 end)
 
 
@@ -78,7 +97,16 @@ AddEventHandler('playerConnecting', function(playerName, setKickReason, deferral
            if count == 0 then
                MySQL.Async.execute('INSERT INTO inventory (identifier, inventory) VALUES (@identifier, @inventory)', {
                    ['@identifier'] = identifier,
-                   ['@inventory'] = json.encode({})
+                   ['@inventory'] = json.encode({
+                    holster1 = "", holster2 = "", holster3 = "", holster4 = "", holster5 = "",
+                    holster6 = "", holster7 = "", holster8 = "", holster9 = "", holster10 = "",
+                    slot1 = "", slot2 = "", slot3 = "", slot4 = "", slot5 = "", slot6 = "",
+                    slot7 = "", slot8 = "", slot9 = "", slot10 = "", slot11 = "", slot12 = "",
+                    slot13 = "", slot14 = "", slot15 = "", slot16 = "", slot17 = "", slot18 = "",
+                    slot19 = "", slot20 = "", slot21 = "", slot22 = "", slot23 = "", slot24 = "",
+                    slot25 = "", slot26 = "", slot27 = "", slot28 = "", slot29 = "", slot30 = "",
+                    slot31 = "", slot32 = "", slot33 = "", slot34 = "", slot35 = "", slot36 = ""
+                })
                }, function(rowsChanged)
                    if rowsChanged > 0 then
                        print("Player record created for SteamID:", identifier)
